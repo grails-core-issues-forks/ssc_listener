d8becb714209b91dba9558c1dfd2a9f1f667c688
sdelamo <sergio.delamo@softamo.com>
2017-10-06 07:40:53 +0200

    add logging statement

--- grails-app/services/com/nationwide/workspace/WorkspaceService.groovy
+++ grails-app/services/com/nationwide/workspace/WorkspaceService.groovy
@@ -2,11 +2,14 @@ package com.nationwide.workspace
 
 import demo.User
 import grails.transaction.Transactional
+import groovy.util.logging.Slf4j
 
+@Slf4j
 @Transactional
 class WorkspaceService {
 
     void setupSessionAfterLogin(String username) {
         User u = User.findByUsername(username)
+        log.info ( 'Username retrieved from database: {}', u.username)
     }
 }
